0a45fe15ae0a214316ac832dc74a05091f8b33d6
Gabr1el20 <6327@holbertonstudents.com>
2023-05-26 06:51:24 -0700

    Fixed multiple references

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
 void
-print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-void
 swap(int *a, int *b, int *array, size_t size)
 {
 	int temp = *a;
