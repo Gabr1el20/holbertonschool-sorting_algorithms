8557f8f8cfd246dfbfff375842e380e0c7c474db
Gabr1el20 <6327@holbertonstudents.com>
2023-05-26 06:48:32 -0700

    Added quicksort

---
+++ 3-quick_sort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+void
+print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
+void
+swap(int *a, int *b, int *array, size_t size)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+	print_array(array, size);
+}
+int
+lomuto(int *array, size_t size, int low, int high)
+{
+	int pi = array[high];
+	int i = (low - 1), j;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pi)
+		{
+			i++;
+			if (i != j && i >= 0)
+				swap(&array[i], &array[j], array, size);
+		}
+	}
+	if (array[high] != array[i + 1])
+		swap(&array[i + 1], &array[high], array, size);
+	return (i + 1);
+}
+void
+recursion(int *array, size_t size, int low, int high)
+{
+	int pivot;
+
+	if (high > low)
+	{
+		pivot = lomuto(array, size, low, high);
+		recursion(array, size, low, pivot - 1);
+		recursion(array, size, pivot + 1, high);
+	}
+}
+void
+quick_sort(int *array, size_t size)
+{
+	int low = 0, high = size - 1;
+	/* se llama a la funcion encargada de la recursion */
+	recursion(array, size, low, high);
+}
